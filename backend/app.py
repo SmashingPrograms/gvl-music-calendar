28ddffdd469a84791f5304a9edfa1cf196309df7
SmashingPrograms <smashingprograms@gmail.com>
2023-05-07 19:46:43 -0500

    Flask backend shows event data

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify
+from get_events import get_events
 import sqlite3
 
 app = Flask(__name__)
+
+events = get_events()
 
 conn = sqlite3.connect('db/data.db')
 conn.close()
 
 @app.route('/api', methods=['GET'])
 def hello_world():
-    data = {'message': 'Hello, World!'}
-    return jsonify(data)
+    data = events
+    return jsonify(events)
 
 if __name__ == '__main__':
     app.run()
